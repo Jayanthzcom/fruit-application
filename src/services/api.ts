cafcbb486becfe5e1c2522bf26e65773a44f9698
Jayanthzcom <talk2eshwar@gmail.com>
2024-10-28 17:31:57 -0400

    netlify build files

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_URL = 'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/';
+//const API_URL = 'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/';
 
 //for local change it to /api
 
 export const fetchFruits = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response =await axios.get('/.netlify/functions/fetchFruits');
     return response.data;
   } catch (error) {
     console.error("Error fetching fruits:", error);
