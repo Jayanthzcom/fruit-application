b7faa6c1e7b817a1896837dde17e4891a773e818
Jayanthzcom <talk2eshwar@gmail.com>
2024-10-28 23:24:02 -0400

    CORS issue fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-//const API_URL = 'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/';
+//for local/prod botht to work dont use this const API_URL = 'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/';
 
-//for local change it to /api
+const API_URL = `https://api.allorigins.win/get?url=${encodeURIComponent('https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/')}`;
+
 
 export const fetchFruits = async () => {
   try {
-    const response = await axios.get('/api');
-    return response.data;
+    const response = await axios.get(API_URL);
+    return  JSON.parse(response.data.contents);
+   
 
   } catch (error) {
     console.error("Error fetching fruits:", error);
